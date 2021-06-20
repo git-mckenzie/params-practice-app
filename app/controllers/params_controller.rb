7182071e7d3cd82955dde499b0ec9649831b8b6b
Jason McKenzie <jmac.atx@gmail.com>
2021-06-20 11:33:34 -0500

    adds broken segment route and method

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -3,4 +3,15 @@ class ParamsController < ApplicationController
     phrase = params["response"].upcase
     render json: phrase.as_json
   end
+
+  def seg_cap
+    phrase = params["response"]
+    render json: { message: "The url segment is #{phrase}" }
+  end
+
+  # def segment_params
+  #   phrase = params["this_is_a_key"]
+  #   render json: { message: "The url segment is #{phrase}" }
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/query_params" => "params#capitalize"
+
+  get "/segment_params/:phrase" => "params#seg_cap"
 end
